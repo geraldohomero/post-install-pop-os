6bfc7fd959574cd7eaae8fe294a2139e8628cf19
Geraldo Homero <geraldohomero@protonmail.com>
2024-02-15 08:40:03 -0300

    Update syncthingStatus.sh - add color

--- syncthingStatus.sh
+++ syncthingStatus.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Check if Syncthing is running using pgrep to verify the ID
+RED='\e[1;91m'
+GREEN='\e[1;92m'
+
+# Check if Syncthing is running
 if pgrep -x "syncthing" > /dev/null
 then
-    echo "Syncthing is running"
+    echo -e "${GREEN}[INFO] - Syncthing is running. ${NO_COLOR}"
 else
-    echo "Syncthing is not running"
+    echo -e "${RED}[INFO] - Syncthing is NOT running. ${NO_COLOR}"
 fi
